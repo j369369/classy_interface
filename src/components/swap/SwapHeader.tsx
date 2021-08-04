926d2b0c78fe6972d10694e84b720d778844219b
scarlet <xzye123@naver.com>
2021-08-04 17:26:07 +0900

    swap commit

--- src/components/swap/SwapHeader.tsx
+++ src/components/swap/SwapHeader.tsx
@@ -2,22 +2,38 @@ import React from 'react'
 import styled from 'styled-components'
 import Settings from '../Settings'
 import { RowBetween } from '../Row'
-import { TYPE } from '../../theme'
+//import { TYPE } from '../../theme'
 
 const StyledSwapHeader = styled.div`
   padding: 12px 1rem 0px 1.5rem;
   margin-bottom: -4px;
   width: 100%;
-  max-width: 420px;
-  color: ${({ theme }) => theme.text2};
+`
+
+const SwapHeaderTitle = styled.strong`
+  color: var(--blue-05);
+  font-size: 1rem;
+  font-weight: 500;
+`
+
+const Setting = styled.span`
+  color: var(--blue-05);
+  font-weight: 500;
+
+  svg > * {
+    color: var(--blue-05);
+    fill: var(--blue-05);
+  }
 `
 
 export default function SwapHeader() {
   return (
     <StyledSwapHeader>
       <RowBetween>
-        <TYPE.black fontWeight={500}>Swap</TYPE.black>
-        <Settings />
+        <SwapHeaderTitle>Trade tokens in a instant</SwapHeaderTitle>
+        <Setting>
+          <Settings />
+        </Setting>
       </RowBetween>
     </StyledSwapHeader>
   )
