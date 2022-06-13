a582cf998a608567bf90bfc7fa65eff1af26de23
ljw559 <ljw559@naver.com>
2022-06-13 12:33:23 +0900

    tokenlist update

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -17,12 +17,16 @@ const CMC_ALL_LIST = 'defi.cmc.eth'
 const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const KLEROS_LIST = 't2crtokens.eth'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
+// const POLYGON_LIST = 'https://unpkg.com/quickswap-default-token-list@1.2.36/build/quickswap-default.tokenlist.json'
+const POLYGON_LIST = 'https://api-polygon-tokens.polygon.technology/tokenlists/default.tokenlist.json'
+// const MUMBAI_LIST = 'https://api-polygon-tokens.polygon.technology/tokenlists/testnet.tokenlist.json'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
- 
+  POLYGON_LIST,
+  // MUMBAI_LIST,
   ...UNSUPPORTED_LIST_URLS // need to load unsupported tokens as well
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [GEMINI_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [POLYGON_LIST]
